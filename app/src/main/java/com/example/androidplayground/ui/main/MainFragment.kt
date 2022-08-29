9079f2dc7c0e7d1e96525dd4ae217781db962bbe
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2022-08-29 16:10:18 +0900

    livedata example

--- app/src/main/java/com/example/androidplayground/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androidplayground/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.androidplayground.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+        private val TAG = MainFragment::class.java.simpleName
     }
 
     private lateinit var viewModel: MainViewModel
@@ -23,10 +25,20 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        Log.d(TAG, "onViewCreated")
+
+        Log.d(TAG, "viewModel.defaultValue.observe(viewLifecycleOwner)")
+        viewModel.defaultValue.observe(viewLifecycleOwner) {
+            Log.d(TAG, "defaultValue LiveData#onChanged is called")
+        }
+        Log.d(TAG, "viewModel.noDefaultValue.observe")
+        viewModel.noDefaultValue.observe(viewLifecycleOwner) {
+            Log.d(TAG, "noDefaultValue LiveData#onChanged is called")
+        }
+    }
 }
--- app/src/main/java/com/example/androidplayground/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/androidplayground/ui/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.androidplayground.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _defaultValue = MutableLiveData<Boolean>(false)
+    val defaultValue: LiveData<Boolean> = _defaultValue
+    private val _noDefaultValue = MutableLiveData<Boolean>()
+    val noDefaultValue: LiveData<Boolean> = _noDefaultValue
 }
